24ab587d6b0c0bf444e6b13ebc628c17ff11a8e2
apsvitor <61666654+apsvitor@users.noreply.github.com>
2020-03-22 19:24:36 -0300

    Add files via upload

--- uVa_10986.cpp
+++ uVa_10986.cpp
@@ -2,49 +2,58 @@
 using namespace std;
 #define INF 1e7
 typedef pair <int,int> ii;
+typedef pair <int,ii> iii;
 int main()
 {
-	int N;
-	cin >> N;
-	for(int casos = 1; casos<=N; casos++)
+	int tests;
+	cin >> tests;
+	while(tests--)
 	{
-		
-		int servers,cables,sender,receiver;
-		cin >> servers >> cables >> sender >> receiver;
-		vector <ii> adjList[50000];
-		int s,r,w;
-		for (int i=0; i<cables; i++)
-		{
-			cin >> s >> r >> w;
-			adjList[s].push_back(ii(w,r));
-			adjList[r].push_back(ii(w,s));
-		}
-		vector <int> dist(servers,INF);
-		dist[sender] = 0;
-		priority_queue < ii, vector<ii>, greater<ii> > pq; // distancia, vertice.
-		pq.push(ii(0,sender));
-		while(!pq.empty())
-		{
-			int auxD = pq.top().first;
-			int auxV = pq.top().second;
-			pq.pop();
-			if  (auxD > dist[auxV])
-				continue;
-			for (unsigned int i = 0; i<adjList[auxV].size(); i++)
+		//source = 0,0		destination = n-1,m-1
+		int N,M;
+		cin >> N >> M;
+		int adjMatrix[N][M];
+		int dist[N][M];
+		for (int i=0;i<N;i++)
+		{	for(int j=0;j<M;j++)
 			{
-				int d = adjList[auxV][i].first;
-				int v = adjList[auxV][i].second;
-				if  (dist[auxV] + d < dist[v])
-				{
-					dist[v] = dist[auxV] + d;
-					pq.push(ii(dist[v], v));
-				}
+				cin >> adjMatrix[i][j];
+				dist[i][j] = INF;
 			}
 		}
-		cout << "Case #" << casos << ": ";
-		if  (dist[receiver] != INF)
-			cout << dist[receiver] << endl;
-		else
-			cout << "unreachable" << endl;
+		dist[0][0] = adjMatrix[0][0];
+		priority_queue < iii, vector <iii>, greater<iii> > pq; // dist, lin, col
+		pq.push(iii(dist[0][0],ii(0,0)));
+		while(!pq.empty())
+		{
+			iii aux = pq.top();
+			pq.pop();
+			int D = aux.first;
+			int Lin = aux.second.first;
+			int Col = aux.second.second;
+			if  (D > dist[Lin][Col])
+				continue;
+			if  (Col+1<M && D + adjMatrix[Lin][Col+1] < dist[Lin][Col+1])		//direita
+			{
+				dist[Lin][Col+1] = D + adjMatrix[Lin][Col+1];
+				pq.push(iii(dist[Lin][Col+1], ii(Lin,Col+1)));
+			}
+			if  (Lin+1<N && D + adjMatrix[Lin+1][Col] < dist[Lin+1][Col]) 		//baixo
+			{
+				dist[Lin+1][Col] = D + adjMatrix[Lin+1][Col];
+				pq.push(iii(dist[Lin+1][Col],ii(Lin+1,Col)));
+			}
+			if  (Col-1>=0 && D + adjMatrix[Lin][Col-1] < dist[Lin][Col-1])		//esquerda
+			{
+				dist[Lin][Col-1] = D + adjMatrix[Lin][Col-1];
+				pq.push(iii(dist[Lin][Col-1],ii(Lin,Col-1)));
+			}
+			if  (Lin-1>=0 && D + adjMatrix[Lin-1][Col] < dist[Lin-1][Col])		//cima
+			{
+				dist[Lin-1][Col] = D + adjMatrix[Lin-1][Col];
+				pq.push(iii(dist[Lin-1][Col],ii(Lin-1,Col)));
+			}
+		}
+		cout << dist[N-1][M-1] << endl;
 	}
 }
